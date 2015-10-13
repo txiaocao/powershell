95a30cbdcc87003b0dd31f4393122ae1434a39e1
txiaocao <txiaocao@126.com>
2015-10-13 19:40:00 +0800

    update

---
+++ 数据抽取.ps1
@@ -0,0 +1,8 @@
+﻿$file = Get-Content .\file.txt;
+
+$ar = New-Object -TypeName System.Collections.ArrayList;
+foreach($item in $file){
+$ar.Add($item.Substring(6,4));
+$ar.Sort();
+}
+$ar|out-file x.txt
---
+++ 清除无用的缓存文件.ps1
@@ -0,0 +1,5 @@
+﻿Remove-Item -Recurse C:\xiaok\svn\facebuy\createpic\app\storage\views\*
+Remove-Item -Recurse C:\xiaok\svn\facebuy\createpic\app\storage\userdata\after\*
+Remove-Item -Recurse C:\xiaok\svn\facebuy\createpic\app\storage\userdata\base\*
+Remove-Item -Recurse C:\xiaok\svn\facebuy\createpic\app\storage\userdata\raw\*
+Remove-Item -Recurse C:\xiaok\svn\facebuy\createpic\data\weblogs\weixin.8864.com\*
