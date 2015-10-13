dca9713726cd2a4987a5fb7ee8af5c9abc920b81
txiaocao <txiaocao@126.com>
2015-10-13 14:47:52 +0800

    base
    base

---
+++ git添加空目录.ps1
@@ -0,0 +1,10 @@
+﻿# GIT项目，空目录添加
+$dir = Get-ChildItem -Directory -Recurse
+
+foreach($item in $dir){
+    $gitfile =  ($item.FullName+"\.gitignore");
+
+    if( $item.GetFiles().Count -eq 0 -and $item.GetDirectories().Count -eq 0){
+        Out-File $gitfile -Encoding utf8
+    }
+}
